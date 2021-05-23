9903f0a0c74615b2bfee1bb4e264207013782be6
DStreety90 <>
2021-05-23 22:37:25 +0000

    my life x2

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -49,7 +49,7 @@ const simpleScore = (word) => {
 }
 const vowelBonusScore = (word) => {
   word = word.toUpperCase();
-  let totalScore = "";
+  let totalScore = 0;
   // testing alt ways?
 for (let i = 0; i < (word.length); i++) {
     if (word[i].includes("A") || word[i].includes("E") || word[i].includes("I") || word[i].includes("O") || word[i].includes("U")) {
